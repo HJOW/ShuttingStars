0750e12c8344f435a6615a4f640f6edcda3bfcaa
HJOW <hujinone22@naver.com>
2026-05-17 22:48:36 +0900

    상당수 버그 수정

--- src/main/webapp/resources/js/shuttingstars.js
+++ src/main/webapp/resources/js/shuttingstars.js
@@ -5,6 +5,7 @@
 /* 게임 기동 근간을 이루는 전역 객체 */
 class ShuttingStarsCore {
     resolution = {w : 1280, h : 720}; // 게임 내 무대의 절대크기
+    dark = true;
     keyList = ['S', 'D', 'F', 'H', 'J', 'K']; // 입력 키
 
     canvas = null; // 캔버스 객체
@@ -12,7 +13,7 @@ class ShuttingStarsCore {
 
     frameTime = 16; // 1000 / 60
     timeMultiplier = 8.0;
-    stageRows = 32; // 스테이지의 세로를 32등분하여 패턴의 시간과 매칭
+    stageRows = 256; // 스테이지의 세로를 N등분하여 패턴의 시간과 매칭
 
     lastObjectId = 0; // 객체 ID 부여용 카운터
     objects = []; // 게임의 구성 요소 객체들 (렌더링 대상)
@@ -20,7 +21,7 @@ class ShuttingStarsCore {
 
     elapsedTime = 0; // 진행 시간 (실제 시간과 단위가 다르며, 곡마다 속도가 다름, 곡의 패턴 배열의 N번째 숫자에 해당)
     timeProgressKey = null; // 시간 진행 타이머 키가 들어가는 변수
-    
+
     point = 0;
     combo = 0;
     report = {
@@ -31,6 +32,10 @@ class ShuttingStarsCore {
 
     song = null; // 현재 플레이 중인 곡, ShuttingStarsSong 객체
     songs = []; // 선택 가능한 곡들, ShuttingStarsSong 객체 배열
+
+    paused = false;
+    resumingTime = 0;
+    simultaneousWorkCycle = 0;
     
     /** 초기화 (게임이 출력될 div 영역 객체를 입력) */
     init(rootDiv) {
@@ -48,27 +53,33 @@ class ShuttingStarsCore {
         htmls += "</div>                                   \n";
         rootDiv.innerHTML = htmls;
         
-        const canvas = document.getElementsByClassName('shuttingstars_canvas_root')[0];
+        const canvas = document.getElementsByClassName('shuttingstars_canvas')[0];
         canvas.style.minWidth  = '1280px';
         canvas.style.minHeight = '720px';
-        this.init(canvas);
-    }
 
-    /** 초기화 (게임이 출력될 canvas 객체를 입력) */
-    init(canvas) {
         this.canvas = canvas;
-        this.ctx    = canvas.getContext('2d');
+        this.ctx = canvas.getContext('2d');
 
         this.canvas.width  = this.resolution.w;
         this.canvas.height = this.resolution.h;
 
         const selfs = this;
+
+        // 반복 처리 프로세스 2개 (렌더링, 공통 동시처리 프로세스) 시작 (곡 동시처리 프로세스는 곡 초기화 시 진행)
         setInterval(() => { selfs.render(); }, this.frameTime);
+        setInterval(() => { selfs.simultaneousWork(selfs.simultaneousWorkCycle); selfs.simultaneousWorkCycle++; if(selfs.simultaneousWorkCycle >= 10000) selfs.simultaneousWorkCycle = 0; }, 20);
 
         document.addEventListener('keydown', (event) => {
-            const key = event.key.toUpperCase();
-            selfs.handleKeyInput(key);
+            selfs.handleKeyInput(event.key.toUpperCase());
         });
+
+        let vGap = window.outerHeight - window.innerHeight;
+        if(vGap < 0) vGap = 0;
+        const fResize = function() {
+            selfs.canvas.style.height = (window.outerHeight - vGap + 1) + 'px';
+        };
+        fResize();
+        window.addEventListener('resize', fResize);
 
         this.resetStage();
     }
@@ -98,11 +109,30 @@ class ShuttingStarsCore {
 
         if(this.song == null) return;
         this.elapsedTime = 0;
+
+        // 곡 동시처리 프로세스 시작
         this.timeProgressKey = setInterval(() => { selfs.timeElapse(); }, Math.round((60000 / this.song.bpm) / this.timeMultiplier));
     }
 
     /** 키 입력 처리 */
     handleKeyInput(key) {
+        // 특수 키 확인
+        if(key == 'ARROWUP' || key == 'ARROWDOWN' || key == 'ARROWLEFT' || key == 'ARROWRIGHT' || key == 'ENTER') {
+            // TODO
+
+        } else if(key == 'ESCAPE') {
+            if(this.state == 'playing') {
+                if(this.paused) {
+                    this.resumingTime = 40;
+                    this.paused = false;
+                } else {
+                    this.paused = true;
+                }
+            }
+        }
+
+        if(this.keyList.indexOf(key) < 0) return;
+
         let idx;
         let notePlacer = null; // 해당 키에 맞는 NotePlacer 를 찾아야 함
         for(idx=0; idx<this.notePlacers.length; idx++) {
@@ -111,15 +141,7 @@ class ShuttingStarsCore {
                 break;
             }
         }
-        if(notePlacer == null) {
-            // 방향키와 엔터 (메뉴에서 사용)
-            if(key == 'ARROWUP' || key == 'ARROWDOWN' || key == 'ENTER') {
-                // TODO
-
-            } else {
-                return; // 해당 키에 맞는 NotePlacer 가 없는 경우, 무시
-            }
-        }
+        if(notePlacer == null) return;
 
         // NotePlacer 폭발 처리
         notePlacer.explosing = 1;
@@ -128,12 +150,7 @@ class ShuttingStarsCore {
         for(idx=0; idx<this.objects.length; idx++) {
             const obj = this.objects[idx];
             if((obj instanceof Note)) {
-                if(obj.explosing >= 10) { // 폭발 완료
-                    // 충돌한 Note 제거
-                    this.objects.splice(idx, 1);
-                    idx--;
-                }
-
+                if(obj.explosing >= 1) continue;
                 if(obj.explosing <= 0 && notePlacer.isConflictedVertical(obj)) {
                     // 해당 NotePlacer 과 충돌한 Note 가 얼마나 위치가 동일한지 판정 (수직으로만 이동하므로 y 좌표 및 노트 크기만 영향) - 백분율 사용
                     const distance = Math.abs(((obj.y - notePlacer.y) * 100.0) / obj.r);
@@ -144,6 +161,7 @@ class ShuttingStarsCore {
                     this.displayResultMark(resultMark);
 
                     obj.explosing = 1; // 폭발 시작
+                    this.objects[idx] = obj;
                 }
             }
         }
@@ -171,21 +189,21 @@ class ShuttingStarsCore {
             this.combo++;
             this.report.PERFECT++;
 
-            this.hp += combo;
+            this.hp += this.combo;
             if(this.hp > 100) this.hp = 100;
         } else if(resultMark == 'GREAT') {
             this.point += (70 * (this.combo + 1));
             this.combo++;
             this.report.GREAT++;
 
-            this.hp += (combo / 2.0);
+            this.hp += (this.combo / 2.0);
             if(this.hp > 100) this.hp = 100;
         } else if(resultMark == 'GOOD') {
             this.point += (50 * (this.combo + 1));
             this.combo++;
             this.report.GOOD++;
 
-            this.hp += (combo / 4.0);
+            this.hp += (this.combo / 4.0);
             if(this.hp > 100) this.hp = 100;
         } else if(resultMark == 'BAD') {
             this.combo = 0;
@@ -203,28 +221,10 @@ class ShuttingStarsCore {
     /** 판정 띄우기 */
     displayResultMark(resultMark) {
         if(resultMark == null) return;
-        
-        // 화면에 판정 결과 띄우기
-        this.ctx.font = '20px Nanum Gothic Coding';
-        if(resultMark == 'MISS') this.ctx.fillStyle = 'rgba(230, 80, 80, 0.8)'; // red
-        else if(resultMark == 'BAD') this.ctx.fillStyle = 'rgba(230, 180, 80, 0.8)'; // purple
-        else if(resultMark == 'GOOD') this.ctx.fillStyle = 'rgba(230, 230, 80, 0.8)'; // yellow
-        else if(resultMark == 'GREAT') this.ctx.fillStyle = 'rgba(180, 230, 80, 0.8)'; // green
-        else if(resultMark == 'PERFECT') this.ctx.fillStyle = 'rgba(80, 230, 80, 0.8)'; // blue
-
-        let midX = this.resolution.w / 2;
-        let midY = this.resolution.h / 2;
-        this.ctx.fillText(resultMark, this.convertX(midX), this.convertY(midY)); // 화면 중앙에 출력
-
-        // 콤보 띄우기
-        if(this.combo > 1) {
-            this.ctx.font = '15px Nanum Gothic Coding';
-            this.ctx.fillStyle = 'rgba(230, 230, 230, 0.8)'; // white
-            this.ctx.fillText('COMBO ' + this.combo, this.convertX(midX), this.convertY(midY + 30)); // 판정 결과 아래에 출력
-        }
+        this.objects.push(new JudgeMark(resultMark));
     }
 
-    /** 화면에 객체들 출력 */
+    /** 화면에 객체들 출력, 동시 반복 호출되며 init 에서 시작됨 */
     render() {
         // 캔버스 비우기
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -247,25 +247,29 @@ class ShuttingStarsCore {
         for(let idx=0; idx<this.objects.length; idx++) {
             const obj = this.objects[idx];
             obj.draw(this.ctx);
-
-            if(obj.id == 9) console.log(obj);
         }
 
         // HP바 그리기
         this.renderHpBar();
 
-        // NoteKeyObject 처리중 애니메이션 재생 진행상황 증가
-        for(let idx=0; idx<this.objects.length; idx++) {
-            const obj = this.objects[idx];
-            if(obj instanceof NoteKeyObject) {
-                if(obj.explosing >= 1 && obj.explosing < 10) {
-                    obj.explosing++;
-                }
+        // 일시정지 상태 그리기
+        if(this.paused) { // TODO
+            let fontSize = 20;
+            this.ctx.font = fontSize + 'px Nanum Gothic Coding';
 
-                if((obj instanceof NotePlacer) && obj.explosing >= 10) {
-                    obj.explosing = 0; // NotePlacer 는 애니메이션이 끝나면 다시 원래 상태로 돌아감
-                }
-            }
+            if(this.dark) this.ctx.strokeStyle = 'rgba(200, 200, 200, 0.9)';
+            else          this.ctx.strokeStyle = 'rgba(80, 80, 80, 0.9)';
+            this.ctx.strokeText('PAUSED', this.convertX(this.resolution.w / 2 - fontSize * (6/2)), this.convertY(this.resolution.h / 2));
+        }
+
+        // 일시정지 해제 대기시간 그리기
+        if(this.resumingTime >= 1) {
+            let fontSize = 12;
+            this.ctx.font = fontSize + 'px Nanum Gothic Coding';
+
+            if(this.dark) this.ctx.strokeStyle = 'rgba(200, 200, 200, 0.9)';
+            else          this.ctx.strokeStyle = 'rgba(80, 80, 80, 0.9)';
+            this.ctx.strokeText('Resume in ' + this.resumingTime + ' cycle !', this.convertX(this.resolution.w / 2 - fontSize * (20/2)), this.convertY(this.resolution.h / 2));
         }
     }
 
@@ -318,8 +322,44 @@ class ShuttingStarsCore {
         this.ctx.fillRect(_shuttingstarcore.convertX(this.notePlacers[0].x - this.notePlacers[0].r), _shuttingstarcore.convertY(10), _shuttingstarcore.convertX(hpBarWidth), _shuttingstarcore.convertY(hpBarHeight));
     }
 
-    /** 시간 진행 (해당 곡의 timeMultiplier 분의 1비트) */
+    /*** 공통 동시처리 프로세스 (init 에서 호출) */
+    simultaneousWork(simultaneousWorkCycle) {
+        // 항상 처리할 사항
+        for(let idx=0; idx<this.objects.length; idx++) {
+            const obj = this.objects[idx];
+            if((obj instanceof JudgeMark) || (obj instanceof Note)) {
+                if(obj.explosing >= 8) {
+                    this.objects.splice(idx, 1);
+                    idx--;
+                }
+            } else if(obj instanceof NotePlacer) {
+                if(obj.explosing >= 8) obj.explosing = 0;
+            }
+        }
+
+        // 일시정지 및 재개 대기 타이밍 처리
+        if(this.paused) return;
+        if(this.resumingTime >= 1) {
+            this.resumingTime--;
+            return;
+        }
+
+        // NoteKeyObject 처리중 애니메이션 재생 진행상황 증가
+        for(let idx=0; idx<this.objects.length; idx++) {
+            const obj = this.objects[idx];
+            if((obj instanceof NoteKeyObject) || (obj instanceof JudgeMark)) {
+                if(obj.explosing >= 1 && obj.explosing < 8) {
+                    if(simultaneousWorkCycle % 4 == 0) obj.explosing++;
+                }
+            }
+        }
+    }
+
+    /** 곡 동시처리 프로세스 - 시간 진행 (해당 곡의 timeMultiplier 분의 1비트) */
     timeElapse() {
+        if(this.paused) return;
+        if(this.resumingTime >= 1) return;
+
         const song = this.song;
         let idx;
         if(song == null) {  this.state = 'menu'; return; } // 곡이 선정되지 않은 경우, 시간 진행 없음
@@ -341,7 +381,7 @@ class ShuttingStarsCore {
             // 패턴이 존재하는 경우, 해당 패턴에 따라 Note 생성
             const note = new Note(patternNow.locationIndex);
             note.speedY = song.getNoteMoveSpeed();
-            note.y = song.getNoteMoveSpeed() * this.stageRows; // 곡의 비트가 여기서 32번이 더 진행되었을 때 NotePlacer 에 도달하는 위치
+            note.y = song.getNoteMoveSpeed() * this.stageRows; // 곡의 비트가 여기서 stageRows 번이 더 진행되었을 때 NotePlacer 에 도달하는 위치
 
             note.id = this.lastObjectId++;
             this.objects.push(note); // 패턴 추가
@@ -350,7 +390,7 @@ class ShuttingStarsCore {
         // 이미 지나가버린 패턴 체크
         for(idx=0; idx<this.objects.length; idx++) {
             const obj = this.objects[idx];
-            if((obj instanceof Note) && (obj.y < (obj.r / -2.0))) {
+            if((obj instanceof Note) && (obj.y <= 0)) {
                 if(obj.explosing == 0) {
                     // 미스 처리
                     let resultMark = 'MISS';
@@ -359,12 +399,6 @@ class ShuttingStarsCore {
 
                     // 폭발 시작
                     obj.explosing = 1;
-                }
-
-                if(obj.explosing >= 10) { // 폭발 완료
-                    // 화면을 지나쳐버린 Note 는 제거, MISS 판정
-                    this.objects.splice(idx, 1);
-                    idx--;
                 }
             }
         }
@@ -396,12 +430,12 @@ class ShuttingStarsCore {
             if(obj instanceof Note) {
                 if(obj.explosing >= 1) continue; // 폭발 중인 Note 는 이동하지 않음
                 obj.y -= obj.speedY;
-                if(obj.y < (obj.r / -2.0)) obj.y = (obj.r / -2.0);
+                if(obj.y < 0) obj.y = 0;
             }
         }
     }
 
-    /** 시간 처리 프로세스 초기화 */
+    /** 곡 동시처리 프로세스 종료 (시작하려면 곡 선택 후 resetStage 가 호출되어야 함) */
     clearTimeHandler() {
         if(this.timeProgressKey != null) {
             clearInterval(this.timeProgressKey);
@@ -416,6 +450,36 @@ class ShuttingStarsCore {
     onSongEnd() {
         // TODO 게임 종료 구현
         _shuttingstarcore.state = 'result';
+    }
+
+    getNotes() {
+        let arr = [];
+        for(let idx=0; idx<this.objects.length; idx++) {
+            const obj = this.objects[idx];
+            if(obj instanceof Note) {
+                arr.push(obj);
+            }
+        }
+        return arr;
+    }
+
+    getNotePlacers() {
+        let arr = [];
+        for(let idx=0; idx<this.objects.length; idx++) {
+            const obj = this.objects[idx];
+            if(obj instanceof NotePlacer) {
+                arr.push(obj);
+            }
+        }
+        return arr;
+    }
+
+    getLeftMargin() {
+        return 20;
+    }
+
+    getTopMargin() {
+        return 0;
     }
     
     /** 게임 내 무대 크기 (resolution.w, resolution.h) 를 실제 화면 내 좌표 (window.outerWidth, window.outerHeight) 로 변환 */
@@ -559,7 +623,7 @@ class NoteKeyObject extends ShuttingStarsObject {
     locationIndex = 0;
     opacity = 1.0;
     dark = false;
-    explosing = 0; // 0 : 일반적인 상황, 그 이상으로 가면 Note 제거 (혹은 Note 제거기 동작) 1~10 : 처리 애니메이션 진행상황
+    explosing = 0; // 0 : 일반적인 상황, 그 이상으로 가면 Note 제거 (혹은 Note 제거기 동작) 1~8 : 처리 애니메이션 진행상황
     constructor(locationIndex) {
         super();
         this.locationIndex = locationIndex;
@@ -567,7 +631,7 @@ class NoteKeyObject extends ShuttingStarsObject {
     }
     /** 기존 투명도 (Opacity) 에 explosing 반영 */
     getNowOpacity() {
-        return this.opacity * (1.0 - (this.explosing / 10.0));
+        return this.opacity * (1.0 - (this.explosing / 8.0));
     }
     draw(ctx) {
         super.draw(ctx);
@@ -585,10 +649,10 @@ class NoteKeyObject extends ShuttingStarsObject {
 /** Note 제거기, 화면 내 고정위치에 떠 있으며, 플레이어가 해당 키 입력 시, 해당 위치를 지나는 노트를 제거하며 점수를 획득함. 또한 노트와 위치가 얼마나 동일한지에 따라 점수 계산 */
 class NotePlacer extends NoteKeyObject {
     constructor(locationIndex) {
-        super();
+        super(locationIndex);
         this.r = 10;
-        this.x = 20 + Math.round(locationIndex * this.r * 2.5);
-        this.y = 40;
+        this.x = 20 + Math.round(locationIndex * this.r * 2.5) + _shuttingstarcore.getLeftMargin();
+        this.y = 40 + _shuttingstarcore.getTopMargin();
         this.key = _shuttingstarcore.keyList[locationIndex];
         this.shape = 'circle';
         this.opacity = 0.2;
@@ -602,13 +666,77 @@ class NotePlacer extends NoteKeyObject {
 class Note extends NoteKeyObject {
     speedY = 10;
     constructor(locationIndex) {
-        super();
+        super(locationIndex);
         this.r = 10;
-        this.x = 20 + Math.round(locationIndex * this.r * 2.5);
-        this.y = _shuttingstarcore.resolution.h;
+        this.x = 20 + Math.round(locationIndex * this.r * 2.5) + _shuttingstarcore.getLeftMargin();
+        this.y = _shuttingstarcore.resolution.h + _shuttingstarcore.getTopMargin();
         this.key = _shuttingstarcore.keyList[locationIndex];
         this.shape = 'circle';
         this.opacity = 0.9;
         this.color = 'rgba(' + this.getColorOfLocationIndex(locationIndex) + ', ' + this.getNowOpacity() + ')';
+    }
+    /** 기존 투명도 (Opacity) 에 explosing 반영 */
+    getNowOpacity() {
+        // this.explosing 반영된 opacity 값 반환
+        // this.opacity 값인 0.9 에서 시작, this.explosing 값이 0~3 까지 증가하는 동안 점진적으로 1.0 까지 증가
+        // 이후 this.explosing 값이 4~8 까지 점진적으로 감소하여 8 이 되면 0이 되어야 함
+        let opacity = this.opacity;
+        if(this.explosing >= 0 && this.explosing <= 3) {
+            opacity += (0.1 * this.explosing); // 0.9 -> 1.0
+        } else if(this.explosing > 3 && this.explosing <= 8) {
+            opacity += (0.4 - (0.08 * (this.explosing - 3))); // 1.0 -> 0.0
+        } else if(this.explosing > 8) {
+            opacity = 0.0;
+        }
+        return opacity;
+    }
+}
+
+/** 판정 글씨와 콤보 마크 */
+class JudgeMark extends ShuttingStarsObject {
+    judgeResult = null; // PERFECT / GREAT / GOOD / BAD / MISS
+    explosing = 0; // 0 : 일반적인 상황, 그 이상으로 가면 이 마크 제거 (혹은 Note 제거기 동작) 1~8 : 처리 애니메이션 진행상황
+    constructor(judgeResult) {
+        super();
+        this.judgeResult = judgeResult;
+    }
+
+    draw(ctx) {
+        // 화면에 판정 결과 띄우기
+        let fontSize = 20;
+        ctx.font = fontSize + 'px Nanum Gothic Coding';
+        if(     this.judgeResult == 'MISS')    ctx.fillStyle = 'rgba(230, 80, 80, 0.8)'; // red
+        else if(this.judgeResult == 'BAD')     ctx.fillStyle = 'rgba(230, 180, 80, 0.8)'; // purple
+        else if(this.judgeResult == 'GOOD')    ctx.fillStyle = 'rgba(230, 230, 80, 0.8)'; // yellow
+        else if(this.judgeResult == 'GREAT')   ctx.fillStyle = 'rgba(180, 230, 80, 0.8)'; // green
+        else if(this.judgeResult == 'PERFECT') ctx.fillStyle = 'rgba(80, 230, 80, 0.8)'; // blue
+
+        let midX = _shuttingstarcore.resolution.w / 2;
+        let midY = _shuttingstarcore.resolution.h / 2;
+        ctx.fillText(this.judgeResult, _shuttingstarcore.convertX(midX), _shuttingstarcore.convertY(midY)); // 화면 중앙에 출력
+
+        // 콤보 띄우기
+        if(_shuttingstarcore.combo > 1) {
+            fontSize = 15;
+            ctx.font = fontSize + 'px Nanum Gothic Coding';
+            if(_shuttingstarcore.dark) ctx.strokeStyle = 'rgba(230, 230, 230, 0.9)';
+            else ctx.strokeStyle = 'rgba(80, 80, 80, 0.9)';
+            ctx.fillText('COMBO ' + _shuttingstarcore.combo, _shuttingstarcore.convertX(midX), _shuttingstarcore.convertY(midY + 30)); // 판정 결과 아래에 출력
+        }
+    }
+
+    /** 기존 투명도 (Opacity) 에 explosing 반영 */
+    getNowOpacity() {
+        return this.opacity * (1.0 - (this.explosing / 8.0));
+    }
+
+    /** 다른 객체와의 충돌 감지 (수평 좌표는 동일하다고 가정하여 수직 충돌만 감지) - 판정 마크는 충돌 없음 */
+    isConflictedVertical(otherObject) {
+        return false;
+    }
+
+    /** 다른 객체와의 충돌 감지 - 판정 마크는 충돌 없음 */
+    isConflicted(otherObject) {
+        return false;
     }
 }
