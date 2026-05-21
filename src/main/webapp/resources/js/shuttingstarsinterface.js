e1703c785de28f01e009dbc743488cc7416f82e6
HJOW <hujinone22@naver.com>
2026-05-22 00:51:39 +0900

    파일에 오픈소스 명시

--- src/main/webapp/resources/js/shuttingstarsinterface.js
+++ src/main/webapp/resources/js/shuttingstarsinterface.js
@@ -1,8 +1,38 @@
 /** 백엔드 서버 / DB 통신 구현 파트, shuttingstars.js 보다 먼저 불러와야 함 */
+/*
+
+LICENSE
+
+Copyright 2026 HJOW (hujinone22@naver.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+ 
+ */
 
 /** 중간 인터페이스 구현 */
 class ShuttingStarsInterface {
-    
+    login(json) {
+        return new Promise((resolve, reject) => { resolve(true); })
+    }
+    listAdditionalSongs() {
+        return new Promise((resolve, reject) => { resolve([]); })
+    }
+    listRankBoard() {
+        return new Promise((resolve, reject) => { resolve([]); })
+    }
+    registerRankRecord(json) {
+        return new Promise((resolve, reject) => { resolve(true); })
+    }
 }
 
 /** Firebase 호스팅 기본제공 API 이용 방식 */
