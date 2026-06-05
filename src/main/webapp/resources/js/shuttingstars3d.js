7f453618bbe7295d161f36c4fdd596519f9cd49b
HJOW <hjow@djict.co.kr>
2026-06-05 11:35:46 +0900

    일부 3D 시각화 구현 중

--- src/main/webapp/resources/js/shuttingstars3d.js
+++ src/main/webapp/resources/js/shuttingstars3d.js
@@ -38,8 +38,114 @@ class ShuttingStars3DModule extends ShuttingStars3DManager {
     mainLight = null;
     gridHelper = null;
     audioVisualizer = null;
+
+    uniforms = {
+        u_time : { value : 0 },
+        u_frequency : {value : 0 }
+    };
+
     init(canvas3d, coreInst) {
+        // DOM 영역에 vertex, fragment 태그 준비 (3D 시각화용, 참고 : https://waelyasmina.net/articles/how-to-create-a-3d-audio-visualizer-using-three-js/)
+        let scriptBlock = document.createElement('script');
+        scriptBlock.id = 'ss_vertexshader';
+        scriptBlock.type = 'vertex';
+        let htmls = `
+        vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+        vec4 mod289(vec4 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+        vec4 permute(vec4 x) { return mod289(((x*34.0)+10.0)*x); }
+        vec4 taylorInvSqrt(vec4 r) { return 1.79284291400159 - 0.85373472095314 * r; }
+        vec3 fade(vec3 t) { return t*t*t*(t*(t*6.0-15.0)+10.0); }
+        float pnoise(vec3 P, vec3 rep) {
+            vec3 Pi0 = mod(floor(P), rep); // Integer part, modulo period
+            vec3 Pi1 = mod(Pi0 + vec3(1.0), rep); // Integer part + 1, mod period
+            Pi0 = mod289(Pi0);
+            Pi1 = mod289(Pi1);
+            vec3 Pf0 = fract(P); // Fractional part for interpolation
+            vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+            vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+            vec4 iy = vec4(Pi0.yy, Pi1.yy);
+            vec4 iz0 = Pi0.zzzz;
+            vec4 iz1 = Pi1.zzzz;
+
+            vec4 ixy = permute(permute(ix) + iy);
+            vec4 ixy0 = permute(ixy + iz0);
+            vec4 ixy1 = permute(ixy + iz1);
+
+            vec4 gx0 = ixy0 * (1.0 / 7.0);
+            vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+            gx0 = fract(gx0);
+            vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+            vec4 sz0 = step(gz0, vec4(0.0));
+            gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+            gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+            vec4 gx1 = ixy1 * (1.0 / 7.0);
+            vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+            gx1 = fract(gx1);
+            vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+            vec4 sz1 = step(gz1, vec4(0.0));
+            gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+            gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+            vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+            vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+            vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+            vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+            vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+            vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+            vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+            vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+            vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+            g000 *= norm0.x;
+            g010 *= norm0.y;
+            g100 *= norm0.z;
+            g110 *= norm0.w;
+            vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+            g001 *= norm1.x;
+            g011 *= norm1.y;
+            g101 *= norm1.z;
+            g111 *= norm1.w;
+
+            float n000 = dot(g000, Pf0);
+            float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+            float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+            float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+            float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+            float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+            float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+            float n111 = dot(g111, Pf1);
+
+            vec3 fade_xyz = fade(Pf0);
+            vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+            vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+            float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+            return 2.2 * n_xyz;
+        }
+        uniform float u_frequency;
+        uniform float u_time;
+        void main() { 
+            float noise = pnoise(position + u_time, vec3(10.));
+            float displacement = (u_frequency / 30.) * (noise / 10.);
+            vec3 newPosition = position + normal * displacement;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0); 
+        }
+        `;
+        scriptBlock.innerHTML = String(htmls).trim();
+        coreInst.contentRoot.append(scriptBlock);
+
+        scriptBlock = document.createElement('script');
+        scriptBlock.id = 'ss_fragmentshader';
+        scriptBlock.type = 'fragment';
+        htmls = `
+        void main() { gl_FragColor = vec4(1.0); }
+        `;
+        scriptBlock.innerHTML = String(htmls).trim();
+        coreInst.contentRoot.append(scriptBlock);
+
+        // 캔버스 크기 구하기
         const canvasBound = canvas3d.getBoundingClientRect();
+
         // 해상도 준비
         this.resolution.w = canvasBound.width;
         this.resolution.h = canvasBound.height;
@@ -313,16 +419,49 @@ class LightPoint extends SphereObject {
     }
 }
 
-class AudioVisualizingObject {
+class AudioVisualizingObject extends ShuttingStars3DObject {
     preparedMeshes = [];
-    constructor() {}
+    vertexShader = null;
+    fragmentShader = null;
+    
+    constructor() {
+        super(); 
+        this.vertexShader = document.getElementById('ss_vertexshader');
+        this.fragmentShader = document.getElementById('ss_fragmentshader');
+    }
 
     onSoundVisualizing(coreInst, manager, audioAnalyzer, audioBuffer) {
+        const selfs = this;
         this.preparedMeshes = [];
         if(audioBuffer == null || audioBuffer.length <= 0) return;
 
         // 흑색 바형 시각화가 나타나는 소스, 좀더 화려한 다른 방법은 없을까?
-        //    그리고 2D 뒤에 나타낼 방법도 고민해야 할 듯
+        //    중앙을 침범하지 않는 타입이 필요함. 그게 아니면, 2D 레이어 뒤에 출력할 방법을 찾아야 함
+
+
+        // https://waelyasmina.net/articles/how-to-create-a-3d-audio-visualizer-using-three-js/ 참고하여 구현 중
+        let avg = 0;
+        for(let idx=0; idx<audioBuffer.length; idx++) {
+            avg += audioBuffer[idx];
+        }
+        avg = avg / audioBuffer.length;
+
+        manager.uniforms.u_time.value = coreInst.elapsedTime;
+        manager.uniforms.u_frequency.value = avg;
+        const uniforms = manager.uniforms; 
+
+        const mat = new THREE.ShaderMaterial({
+            wireframe : true,
+            uniforms,
+            vertexShader : this.vertexShader.textContent,
+            fragmentShader : this.fragmentShader.textContent
+        });
+        const geo = new THREE.IcosahedronGeometry(4, 30);
+        const mesh = new THREE.Mesh(geo, mat);
+        mesh.position.set( coreInst.convertX(coreInst.getStageWidth() / 2), coreInst.convertY(coreInst.getStageHeight), 10);
+        this.preparedMeshes.push(mesh);
+
+
         /*
         const count = audioBuffer.length;
         const width = manager.resolution.w;
