0f10738f87d6b37aac26aa41a8cb409c132981b6
HJOW <hujinone22@naver.com>
2026-07-02 21:07:14 +0900

    webpack 1차 세팅

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "entry" : "./src/main/webapp/resources/js/shuttingstars.js",
+    "output" : {
+        "path" : __dirname + "/src/main/webapp/resources/js/",
+        "filename" : "shuttingstars.bundle.js"
+    }
+}
