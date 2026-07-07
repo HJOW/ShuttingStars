a7d3e75fbcc57a6421ef3576e847b720e4fdaf65
HJOW <hjow@djict.co.kr>
2026-07-07 13:05:54 +0900

    babel 설정 변경

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     "module" : {
         "rules" : [
             {
-                "test" : /\.js$/,
+                "test" : /\.(ts|js)$/,
                 "exclude" : /node_modules/,
                 "use" : {
                     "loader" : "babel-loader"
