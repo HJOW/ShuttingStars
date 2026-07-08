8af1c52458fa665b6723ba89f275a83e0b3d55d6
HJOW <hjow@djict.co.kr>
2026-07-09 08:51:27 +0900

    일부 babel 설정 변경

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,12 @@ module.exports = function(api) {
     api.cache(true);
     return {
         "presets" : [
-            "@babel/preset-typescript",
+            ["@babel/preset-typescript", {
+                "allowDeclareFields" : true
+            }],
             ["@babel/preset-env", {
-                "targets" : "> 0.25%, not dead"
+                "targets" : "> 0.25%, not dead",
+                "bugfixes" : true
             }]
         ],
         "plugins" : []
