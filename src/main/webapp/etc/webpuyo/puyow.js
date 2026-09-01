9f8a0b392e5fae26323e6ba327f91f8f306db591
HJOW <hujinone22@naver.com>
2026-09-01 21:37:49 +0900

    뿌요 W - 사운드 관련

--- src/main/webapp/etc/webpuyo/puyow.js
+++ src/main/webapp/etc/webpuyo/puyow.js
@@ -1198,7 +1198,7 @@
     */
     function loadSoundDataURL(currentSoundURL, save) {
         if(typeof(currentSoundURL) == 'undefined' || currentSoundURL == null) currentSoundURL = soundDataURL;
-        if (soundDataURL === null || soundDataURL === '') return;
+        if (currentSoundURL === null || currentSoundURL === '') return;
         try {
             const convertedURL = convertURL(currentSoundURL);
 
@@ -11748,6 +11748,7 @@
         createSoundPool,
         setEnemySoundPool,
         setCommonSoundPool,
+        loadSoundDataURL,
         applySoundDataJson,
         setStorageManager,
         registerFeverStageState,
